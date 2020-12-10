3a0b29e9401a79f633fb341d9b739d8410726108
Crowdbotics <team@crowdbotics.com>
2020-12-10 16:42:25 +0000

    Update components

--- src/features/BlankScreen0184156/screens/blank.js
+++ src/features/BlankScreen0184156/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,44 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { TextInput_3: "", CheckBox_25: true }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View style={styles.View_1}>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_3}
+        onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
+      />
+      <View />
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_25}
+        onPress={nextChecked => this.setState({ CheckBox_25: nextChecked })}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: { alignSelf: "center" },
+  TextInput_3: {},
+  View_7: {},
+  Button_5: {},
+  CheckBox_25: {}
+})
